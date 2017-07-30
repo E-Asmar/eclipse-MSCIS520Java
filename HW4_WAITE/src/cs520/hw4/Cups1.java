0422868097e5610b4a5a46663a44ef8f0942b589
E-Asmar <PatrickM.Waite@gmail.com>
2017-07-30 19:04:45 -0400

    7/30/2017

--- HW4_WAITE/src/cs520/hw4/Cups1.java
+++ HW4_WAITE/src/cs520/hw4/Cups1.java
@@ -1,12 +1,15 @@
 package cs520.hw4;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
 
 public class Cups1 extends JFrame {
-	private int startX, 	//left most point
-				startY, 	//top most point
+	private int startX,		//left most point
+				workingX,	//to help with placement of cups in stack
+				startY,		//top most point
+				workingY,	//to help with placement of cups in stack
 				cupWidth,	//width of cup
 				cupHeight,	//height of cup
 				baseLength, //number of cups on bottom row
@@ -28,12 +31,40 @@ public class Cups1 extends JFrame {
 	
 	//viewport (paint method)
 	public void paint(Graphics g) {
+		//working x and y to keep start x and y separate this will become important when placing new rows on the pyramid 
+		workingX = startX; 
+		workingY = startY;
 		
+		//first for loop or rows
+		for (int row = baseLength; row >= 1; row--) {
+			//setting the color of the cups to alternating rows
+			if(row%2 == 1) {
+				g.setColor(Color.red);
+			}else {
+				g.setColor(Color.blue);
+			}
+			//creates each cup image in the row as a 'column'
+			for(int col = 0; col < row; col++) {
+				
+				g.fillRect(workingX, workingY, cupWidth, cupHeight);
+				//sets the x value for the next cup to be added
+				workingX = workingX + cupWidth + cupSpaceing;
+				
+			}
+			//sets the y value for the next row
+			workingY = workingY - cupHeight;
+			//sets the offset for the row to make the pyramid shape
+			startX = startX + ((cupWidth + cupSpaceing)/2);
+			//sets workingX to the new startX value for the pattern
+			workingX = startX;
+			//g.setColor(Color.red);
+		}
 	}
 	
 	//main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//sets width and height of JFrame
 		int width = 550;
 		int height = 550;
 		
