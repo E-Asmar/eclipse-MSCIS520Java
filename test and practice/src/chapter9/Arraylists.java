a3420a047abb75508e07a71bab39adb7a7901789
Patrick Waite <patrickm.waite@gmail.com>
2017-07-31 23:25:48 -0400

    commit

--- test and practice/src/chapter9/Arraylists.java
+++ test and practice/src/chapter9/Arraylists.java
@@ -1,10 +1,13 @@
 package chapter9;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Arraylists {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double[] myDoubles = {0.0,1.0,2.0};
 	}
 
 }
